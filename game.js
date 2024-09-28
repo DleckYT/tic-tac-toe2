bdc72b9b6886418427939b800f2f03412ce971be
Lance Godinez <LanceGodinezoliveros@gmail.com>
2024-09-28 19:30:16 +1200

    Adding Game Foundations

--- game.js
+++ game.js
@@ -1,2 +1,33 @@
-console.log("Welcome to Tic-Tac-Toe! Enjoy!")
+const cells = document.getElementsByTagName('TD')
 
+const X = 1
+const O = 2
+const empty = 0
+const state = [_, _, _, _, _, _, _, _, _]
+
+function checkForWin(symbol) {
+  return false
+}
+
+const istalemate = state.every((cell) => cell !== empty)
+const istalemate = !state.some((cell) => cell === empty)
+
+function updateView() {
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i]
+    const s = state[i]
+    if (cells.firstChild) {
+      switch (s) {
+        case X:
+          cell.firstChild.src = './images/x.png'
+          break
+        case O:
+          cell.firstChild.src = './images/o.png'
+          break
+        case _:
+          cell.firstChild.src = './images/empty'
+          break
+      }
+    }
+  }
+}
